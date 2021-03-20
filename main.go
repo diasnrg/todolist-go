a826765a51fe5a66c5d037f49654cb110652a325
vdios <>
2021-03-20 12:27:18 +0600

    saving to .txt file works

--- main.go
+++ main.go
@@ -6,20 +6,11 @@ import(
   "fmt"
   "log"
   "io/ioutil"
-  "os"
 )
 
 type todo struct{
   description string
   status bool
-}
-
-var file *os.File
-var err error
-
-func init(){
-  file, err = os.Create("output.txt")
-  check(err)
 }
 
 func add(w http.ResponseWriter,r *http.Request){
@@ -31,18 +22,12 @@ func save(w http.ResponseWriter,r *http.Request){
   description := r.FormValue("desc")
   newTodo := todo{description,false}
 
-  //saving object to txt file
+  oldBody, err := ioutil.ReadFile("output.txt")
+  check(err)
 
-  //method 1
-    // file, err := os.Create("output.txt")
-    // check(err)
-    file, err = os.Open("output.txt")
-    defer file.Close()
-    _,err = file.WriteString(fmt.Sprintf("%v",newTodo))
-    check(err)
-
-  // err := ioutil.WriteFile("output.txt", []byte(fmt.Sprintf("%v",newTodo)), 0644)
-  // check(err)
+  bodyString := string(oldBody) + fmt.Sprintf("%v\n",newTodo)
+  err = ioutil.WriteFile("output.txt", []byte(bodyString), 0644)
+  check(err)
 
   fmt.Printf("todo %v added\n",newTodo)
 
@@ -51,12 +36,9 @@ func save(w http.ResponseWriter,r *http.Request){
 }
 
 func list(w http.ResponseWriter,r *http.Request){
-  body, err := ioutil.ReadFile("output.txt")
-  check(err)
-  fmt.Println(string(body))
-
+  // body, err := ioutil.ReadFile("output.txt")
+  // check(err)
   //here another template will iterate through items and show them in page
-
 }
 
 func check(err error){
@@ -66,7 +48,7 @@ func check(err error){
 }
 
 func (t todo) String() string{
-  return fmt.Sprintf("%v:%v\n",t.description,t.status)
+  return fmt.Sprintf("%v:%v",t.description,t.status)
 }
 
 func main(){

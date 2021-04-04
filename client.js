2ed3f58c6f3bb01d0b99b9a3097dbb7512de9b73
vdios <diasnurmangaliyev@gmail.com>
2021-04-04 12:00:27 +0600

    bootstrap added, fetch works asyncronous with add, delete methods

--- client.js
+++ client.js
@@ -1,53 +1,57 @@
 const url = 'http://localhost:8090'
-document.querySelector('#btgetlist').addEventListener('click',getlist)
 document.querySelector('#btadd').addEventListener('click',addItemToDB)
 const list = document.querySelector('#list')
+getlist()
 
 async function getlist(){
   list.innerHTML = ''
-  const response = await fetch(url+'/list/')
-  const data = await response.json()
-  const mp = new Map(Object.entries(data))
-  console.log(mp)
-  addItems(mp)
+  const response = await fetch(url+'/list/').then(response => response.json())
+  const data = new Map(Object.entries(response))
+  console.log(data)
+  for(let [id,todo] of data.entries()){
+    addItemToDOM(id,todo)
+  }
 }
 
-function addItemToDB(){
+async function addItemToDB(){
   const input = document.querySelector('#description')
   const description = input.value
   input.value = ''
-  fetch(url+'/add/',{
+  const data = await fetch(url+'/add/',{
     method:'POST',
     headers:{
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({'description':description})
-  }).then(getlist())
+    })
+    .then(response => response.json())
+    getlist()
 }
 
-function addItems(data){
-  for(let [id,todo] of data.entries()){
-    const item = document.createElement('div')
-    const description = document.createElement('div')
-    const btdelete = document.createElement('button')
+function addItemToDOM(id,todo){
+  const item = document.createElement('div')
+  const description = document.createElement('div')
+  const btdelete = document.createElement('button')
 
-    item.id = id
-    description.innerHTML = todo.Description
-    btdelete.textContent = 'delete'
-    btdelete.addEventListener('click',removeItem)
+  item.id = id
+  description.innerHTML = todo.Description
+  btdelete.textContent = 'delete'
+  btdelete.addEventListener('click',removeItem)
 
-    item.appendChild(description)
-    item.appendChild(btdelete)
-    list.appendChild(item)
-  }
+  item.appendChild(description)
+  item.appendChild(btdelete)
+  btdelete.className += 'btn btn-danger'
+  item.className += 'd-flex justify-content-between w-50 p-2 my-4 border border-warning align-items-center rounded'
+  list.appendChild(item)
 }
 
 async function removeItem(item){
   const id = item.target.parentElement.id
-  fetch(url+'/delete/'+id,{
+  const data = await fetch(url+'/delete/'+id,{
     method:'DELETE',
     headers:{
       'Content-Type':'application/json'
     }
-  }).then(getlist())
+  })
+  getlist()
 }
